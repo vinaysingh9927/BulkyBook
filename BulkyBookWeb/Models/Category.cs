f1f16cd8a6daf30c0c03bdc1e7656eca4b0fe725
vinaysingh9927 <vinay.singh@bharuwasolution.com>
2022-04-16 09:48:52 +0530

    Till Database connected

---
+++ BulkyBookWeb/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyBookWeb.Models
+{
+    public class Category
+    {   
+        [Key]        
+        public int ID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+        public DateTime DateTime { get; set; } = DateTime.Now;
+    } 
+}
