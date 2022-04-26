97f4ea45b9bede383f018ce397bf93facb5d87d9
vinaysingh9927 <vinay.singh@bharuwasolution.com>
2022-04-26 18:04:44 +0530

    Product create buttone not work

--- BulkyBookWeb/Areas/Admin/Controllers/ProductController.cs
+++ BulkyBookWeb/Areas/Admin/Controllers/ProductController.cs
@@ -8,8 +8,6 @@ namespace BulkyBookWeb.Controllers
     [Area("Admin")]  //explicity - not required it automatically find
     public class ProductController : Controller
     {
-        //private readonly ApplicationDbContext _db;
-        //private readonly ICoverTypeRepository _db;
         private readonly IUnitOfWork _unitOfWork;
 
         public ProductController(IUnitOfWork db) 
@@ -19,62 +17,40 @@ namespace BulkyBookWeb.Controllers
 
         public IActionResult Index()
         { 
-            IEnumerable<CoverType> objCoverTypeList = _unitOfWork.CoverType.GetAll(); //to use unityof work.
-            return View(objCoverTypeList);
+            IEnumerable<Product> Product = _unitOfWork.Product.GetAll(); //to use unityof work.
+            return View(Product);
         }
-
-        //Create Functionality
-        //GET
-        public IActionResult Create() 
-        {
-            return View();
-        }
-        //Post
-        [HttpPost]
-        [AutoValidateAntiforgeryToken]
-        public IActionResult Create(CoverType obj)  //in validation check model is valid or not (Require properties have or not)
-        {
-            if (ModelState.IsValid)   
-            {
-                _unitOfWork.CoverType.Add(obj);  
-                _unitOfWork.Save();
-                TempData["success"] = "CoverType created successfully";
-                return RedirectToAction("Index"); 
-            }
-            return View(obj);
-        }
-
-
 
         //Edit/Update Functionality 
 
         //GET
-        public IActionResult Edit(int? id) 
+        public IActionResult Upsert(int? id)  //upsert (create/update=upsert)
         {
+            Product product = new();
             if (id ==null || id==0)
             {
-                return NotFound();
+                //create product
+                return NotFound(product);
             }
-            
-            var CoverTypeFromDbFirst = _unitOfWork.CoverType.GetFirstOrDefault(u=>u.Id == id); 
-           
-            if (CoverTypeFromDbFirst == null) 
+            else
             {
-                return NotFound();
+                //update product
             }
-            return View(CoverTypeFromDbFirst); 
+
+            return View(product); 
         }
+        
         //Post
         [HttpPost]
-        [AutoValidateAntiforgeryToken] 
-        public IActionResult Edit(CoverType obj)  //in validation check model is valid or not (Require properties have or not)
+        [AutoValidateAntiforgeryToken]  
+        public IActionResult Upsert(CoverType obj)  //in validation check model is valid or not (Require properties have or not)
         {
             
             if (ModelState.IsValid)
             {
                 _unitOfWork.CoverType.Update(obj); 
                 _unitOfWork.Save(); 
-                TempData["success"] = "CoverType updated successfully";
+                TempData["success"] = "Product updated successfully";
                 return RedirectToAction("Index");
             }
             return View(obj);
@@ -89,13 +65,13 @@ namespace BulkyBookWeb.Controllers
                 return NotFound();
             }
             
-            var CoverTypeFromDbFirst = _unitOfWork.CoverType.GetFirstOrDefault(u=>u.Id == id); 
+            var ProductFromDbFirst = _unitOfWork.CoverType.GetFirstOrDefault(u=>u.Id == id); 
 
-            if (CoverTypeFromDbFirst == null)
+            if (ProductFromDbFirst == null)
             {
                 return NotFound();
             }
-            return View(CoverTypeFromDbFirst); 
+            return View(ProductFromDbFirst); 
         }
 
         //Post
@@ -113,7 +89,7 @@ namespace BulkyBookWeb.Controllers
             _unitOfWork.CoverType.Remove(obj);
          
             _unitOfWork.Save();
-            TempData["success"] = "CoverType deleted successfully";
+            TempData["success"] = "Product deleted successfully";
             return RedirectToAction("Index"); 
         }
     }
