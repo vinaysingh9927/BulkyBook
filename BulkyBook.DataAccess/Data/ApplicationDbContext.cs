4c87b2751dba5b5bf0a26a49ba3cad854a94308c
vinaysingh9927 <vinay.singh@bharuwasolution.com>
2022-04-26 16:04:11 +0530

    Add Product Model Successfully

--- BulkyBook.DataAccess/Data/ApplicationDbContext.cs
+++ BulkyBook.DataAccess/Data/ApplicationDbContext.cs
@@ -14,5 +14,6 @@ namespace BulkyBook.DataAccess
         //each model we have to create DbSet
         public DbSet<Category> Categories { get; set; }
         public DbSet<CoverType> CoverTypes { get; set; }  //CoverTypes table name created in database
-    }
+        public DbSet<Product> Products { get; set; }  //Product table name created in database
+    } 
 }  
